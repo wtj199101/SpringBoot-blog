04cf4b5e28aea5f29f7974f9776add3b3c9f17d7
Miaolei <Miaolei>
2017-06-16 16:30:16 +0800

    '12'

--- src/main/java/cn/springBoot/MyBootApplication.java
+++ src/main/java/cn/springBoot/MyBootApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages={"cn.springBoot"})  //The @SpringBootApplication annotation is equivalent to using @Configuration, @EnableAutoConfiguration and @ComponentScan
 @ServletComponentScan//不加这个filter不生效
-public class MyBootApplication {
+public class MyBootApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(MyBootApplication.class);
         app.setBannerMode(Banner.Mode.OFF);
